0e7416974b1cad9f167b6d0a86ac850642e9a5e5
Dana Kulp <dkulp23@gmail.com>
2016-11-21 23:21:27 -0800

    Completed TODOs but page is not currently rendering. Not sure exactly what I'm missing with the Handlebars syntax.

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -17,12 +17,14 @@ Article.prototype.toHtml = function() {
   //   The result is added to the object as a new property, which can then be referenced
   //   by a key in the template. For example, you might want to display how old a post is,
   //   or say "(draft)" if it has no publication date:
-  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn)) / 60 / 60 / 24 / 1000);
   this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
   // TODO: Use handlebars to render your articles!
   //       - Select your template from the DOM.
   //       - Now "compile" your template with Handlebars.
   //       - Don't forget to return your template for this article.
+  var blackMagicCompile = Handlebars.compile($('#articleTemplate').html());
+  $('#articles').append(blackMagicCompile(ourLocalData));
 };
 
 ourLocalData.sort(function(a,b) {
